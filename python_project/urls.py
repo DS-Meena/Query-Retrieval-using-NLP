5b42f4da60860cd7762f53c6da3cf9a6913afa0e
D_SM <dharamsinghmeena2000@gmail.com>
2020-06-05 22:00:03 +0530

    added the linkdin url

--- python_project/urls.py
+++ python_project/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # path('', quesv.emp, name='emp'),   # for Just Views
     path('', ansv.index, name='index'),
+    path('re_direct', ansv.re_direct, name='re_direct'),
     # path('', ansv.curr_output, name='curr_output'),
     # path('hello/', views.hello),
 ]
